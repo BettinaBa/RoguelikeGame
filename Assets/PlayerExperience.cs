6ac63bcb99ae1cf037264b3e17e879b07fae27ea
BettinaBa <153662054+BettinaBa@users.noreply.github.com>
2025-06-10 19:36:50 +0300

    Add level up system

--- Assets/PlayerExperience.cs
+++ Assets/PlayerExperience.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class PlayerExperience : MonoBehaviour
 {
-    public int experience;
+    public int level = 1;
+    public int experience = 0;
+    public int experienceToNextLevel = 5;
+    public int xpMultiplier = 1;
 
     public void AddExperience(int amount)
     {
-        experience += amount;
+        experience += amount * xpMultiplier;
         Debug.Log("Player XP: " + experience);
+
+        if (experience >= experienceToNextLevel)
+        {
+            if (LevelUpManager.Instance != null)
+                LevelUpManager.Instance.ShowLevelUpOptions();
+
+            experience -= experienceToNextLevel;
+            level++;
+            experienceToNextLevel += 5;
+        }
     }
 }
